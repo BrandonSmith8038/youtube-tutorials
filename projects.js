084566b1b41b4d19d00aeba6b4a78c82c619e1fc
Brandon Smith <brandon@reddirtwebdesign.com>
2017-10-07 10:12:32 +0000

    Local JSON output to home template

---
+++ projects.js
@@ -0,0 +1,14 @@
+const myProjects = {
+    projects: [{
+            "first": "Brandon",
+            "Last": "Smith",
+            "address": "24828 W. Quails Nest Ln."
+ 	}, {
+            "first": "Amber",
+            "Last": "Cahill-Smith",
+            "address": "16277 W. Jackson"
+	}
+ ]
+}
+
+module.exports = myProjects;
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,28 @@
 const express = require('express');
 const router = express.Router();
+const projects = require("../projects.js")
+// const myProjects = {
+//     projects: [{
+//             "first": "Brandon",
+//             "Last": "Smith",
+//             "address": "24828 W. Quails Nest Ln."
+// 	},
+//         {
+
+//             "first": "Amber",
+//             "Last": "Cahill",
+//             "address": "16277 W. Jackson"
+
+// 	}
+
+// ]
+// }
+
 
 
 router.get('/', (req, res) => {
-    res.render('index/home');
+    console.log(projects);
+    res.render('index/home', projects);
 });
 
 router.get('/about', (req, res) => {
