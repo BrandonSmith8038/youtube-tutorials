dfe993d97fff4ef523f2a74a729185c482219776
Brandon Smith <brandon@reddirtwebdesign.com>
2017-10-08 22:58:20 -0700

    Switched to using git hub pages, added new projects

--- routes/projects.js
+++ routes/projects.js
@@ -1,27 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const projects = require("../projects.js")
-// const myProjects = {
-//     projects: [{
-//             "first": "Brandon",
-//             "Last": "Smith",
-//             "address": "24828 W. Quails Nest Ln."
-// 	},
-//         {
-
-//             "first": "Amber",
-//             "Last": "Cahill",
-//             "address": "16277 W. Jackson"
-
-// 	}
-
-// ]
-// }
 
 
-
-router.get('/number-facts/', (req, res) => {
-    res.send('Testing 123');
+router.get('/number-facts456/', (req, res) => {
+    res.sendFile('/projects/youtube-tutorials/views/projects/number-facts1/index.html');
+    res.sendFile('/projects/youtube-tutorials/views/projects/number-facts1/main.js');
 });
 
 
